47de54cbea4539319cf2280f60e6f65c95fb941e
omnia <omnia.z.talaab@gmail.com>
2021-12-08 18:53:44 +0200

    Start with login

--- lib/APIS/model/Update.dart
+++ lib/APIS/model/Update.dart
@@ -1,50 +1,13 @@
-
 class Update {
-  Data ?_data;
 
-  Update({Data ?data}) {
-    this._data = data;
-  }
+  String ?currentVersion;
+  bool ?hasUpdate;
 
-  Data get data => data;
-  set data(Data data) => _data = data;
+
+  Update();
 
   Update.fromJson(Map<String, dynamic> json) {
-    _data = json['data'] != null ? new Data.fromJson(json['data']) : null;
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    if (this._data != null) {
-      data['data'] = this._data?.toJson();
-    }
-    return data;
-  }
-}
-
-class Data {
-  String ?_currentVersion;
-  bool ?_hasUpdate;
-
-  Data({String ?currentVersion, bool ?hasUpdate}) {
-    this._currentVersion = currentVersion;
-    this._hasUpdate = hasUpdate;
-  }
-
-  String get currentVersion => _currentVersion!;
-  set currentVersion(String currentVersion) => _currentVersion = currentVersion;
-  bool get hasUpdate => _hasUpdate!;
-  set hasUpdate(bool hasUpdate) => _hasUpdate = hasUpdate;
-
-  Data.fromJson(Map<String, dynamic> json) {
-    _currentVersion = json['current_version'];
-    _hasUpdate = json['has_update'];
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['current_version'] = this._currentVersion;
-    data['has_update'] = this._hasUpdate;
-    return data;
+    currentVersion = json['current_version'];
+    hasUpdate = json['has_update'];
   }
 }
--- lib/SplashScene.dart
+++ lib/SplashScene.dart
@@ -1,8 +1,13 @@
 import 'dart:async';
+import 'dart:convert';
+import 'package:fastrak/APIS/model/Update.dart';
 import 'package:fastrak/WelcomeScene.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:http/http.dart' as http ;
+import 'package:http/http.dart' as http;
+import 'package:launch_review/launch_review.dart';
+import 'package:url_launcher/url_launcher.dart';
+
 class SplashScene extends StatefulWidget {
   const SplashScene({Key? key}) : super(key: key);
 
@@ -11,33 +16,55 @@ class SplashScene extends StatefulWidget {
 }
 
 class _SplashSceneState extends State<SplashScene> {
+  Future<Update?> getUpdate() async {
+    var response = await http.get(Uri.parse(
+        "http://3.126.221.243:8080/api/v1/configuration/latest-version?platform=ios&app_type=1&version=0.0.2)"));
+    print(response.statusCode);
+    print(response.body);
+    if (response.statusCode == 200) {
+      Update model = Update.fromJson(jsonDecode(response.body)['data']);
+      if (model.hasUpdate == false) {
+        throw Navigator.of(context)
+            .pushReplacement(MaterialPageRoute(builder: (_) => WelcomeScene()));
+      } else {
+        _launchURL();
+      }
+    }
+  }
+
+  _launchURL() async {
+    const url = "https://play.google.com/store/apps/details?id=" +
+        "com.example.fastrak";
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      // throw 'Could not launch $url';
+      throw Navigator.of(context)
+          .pushReplacement(MaterialPageRoute(builder: (_) => WelcomeScene()));
+    }
+  }
 
   @override
   void initState() {
-
     super.initState();
     Timer(Duration(seconds: 5), () {
-      Navigator.of(context)
-          .pushReplacement(MaterialPageRoute(builder: (_) => WelcomeScene()));
+      getUpdate();
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(statusBarColor: Colors.deepPurple));
+    SystemChrome.setSystemUIOverlayStyle(
+        SystemUiOverlayStyle(statusBarColor: Colors.deepPurple));
     return Scaffold(
-      backgroundColor: Colors.deepPurple,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset('images/logo.png'),
-
-
-          ],
-        ),
-      ),
-    );
+        backgroundColor: Colors.deepPurple,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset('images/logo.png'),
+            ],
+          ),
+        ));
   }
-
 }
